0d57a622a832a3f0b38d4e5f1681b58f7c2f1288
Dochkoff <hounds.07cinder@icloud.com>
2022-07-13 14:16:23 +0300

    3_Lab

---
+++ Programming Basics With C#/3_ConditionalStatements/Lab/4_Number1_9toText/4_Number1_9toText.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Number1_9toText
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //ako imame samo if nqkolko puti gi proverqva vsichkite
+            //no ako ima if i sled tova if else mu trqbva samo edno da otgovarq i spira da proverqva nadolu
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number == 1)
+            {
+                Console.WriteLine("one");
+            }
+            else if (number == 2)
+            {
+                Console.WriteLine("two");
+            }
+            else if (number == 3)
+            {
+                Console.WriteLine("three");
+            }
+            else if (number == 4)
+            {
+                Console.WriteLine("four");
+            }
+            else if (number == 5)
+            {
+                Console.WriteLine("five");
+            }
+            else if (number == 6)
+            {
+                Console.WriteLine("six");
+            }
+            else if (number == 7)
+            {
+                Console.WriteLine("seven");
+            }
+            else if (number == 8)
+            {
+                Console.WriteLine("eight");
+            }
+            else if (number == 9)
+            {
+                Console.WriteLine("nine");
+            }
+            else
+            {
+                Console.WriteLine("number too big");
+            }
+        }
+    }
+}
---
+++ Programming Basics With C#/3_ConditionalStatements/Lab/8_AreaOfFigures/8_AreaOfFigures.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace AreaOfFigures
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+
+            double area = 0;
+
+            if (figure == "square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                area = side * side;
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double sideB = double.Parse(Console.ReadLine());
+                area = sideA * sideB;
+            }
+            else if (figure == "circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                area = Math.PI * radius * radius;
+            }
+            else if (figure == "triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double h = double.Parse(Console.ReadLine());
+                area = side * h / 2;
+            }
+            Console.WriteLine($"{area:F3}");
+        }
+    }
+}
