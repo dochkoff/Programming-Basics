08b6d00470c304fbda8d92f1b63243bee83652f1
Dochkoff <hounds.07cinder@icloud.com>
2022-07-19 07:13:26 +0300

    minor

--- Programming Basics With C#/5_WhileLoop/Lab/01_NumberInRange1_100/01_NumberInRange1_100.cs
+++ Programming Basics With C#/5_WhileLoop/Lab/01_NumberInRange1_100/01_NumberInRange1_100.cs
@@ -1,41 +1,22 @@
 ﻿using System;
 
-namespace NumberInRange1_100
+namespace xTest
 {
     class MainClass
     {
         public static void Main(string[] args)
         {
-            int weight = int.Parse(Console.ReadLine());
-            int lenght = int.Parse(Console.ReadLine());
-            int height = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number in range [1...100]: ");
+            int num = int.Parse(Console.ReadLine());
 
-
-            int space = weight * lenght * height;
-            int boxesSum = 0;
-            bool neededSpace = true;
-
-            while (space - boxesSum >= 0)
+            while (num < 1 || num > 100)
             {
-                string command = Console.ReadLine();
-                if (command == "Done")
-                {
-                    neededSpace = false;
-                    break;
-                }
-
-                int boxes = int.Parse(command);
-                boxesSum += boxes;
+                Console.WriteLine("Invalid number!");
+                Console.WriteLine("Enter number in range [1...100]: ");
+                num = int.Parse(Console.ReadLine());
             }
 
-            if (neededSpace == false)
-            {
-                Console.WriteLine($"{space - boxesSum} Cubic meters left.");
-            }
-            else
-            {
-                Console.WriteLine($"No more free space! You need {Math.Abs(space - boxesSum)} Cubic meters more.");
-            }
+            Console.WriteLine($"The number is {num}");
         }
     }
 }
---
+++ Programming Basics With C#/5_WhileLoop/Lab/02_Graduation/02_Graduation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02_Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
