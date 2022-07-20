7d592d6dc3c7f369f73ba16bf9e35224ba2d1108
Dochkoff <hounds.07cinder@icloud.com>
2022-07-21 00:02:39 +0300

    03_Graduate Part II

---
+++ Programming Basics With C#/5_WhileLoop/Lab/03_GraduationPartII/03_GraduationPartII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _02_Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double counter = 1;
+            double sum = 0;
+            double badGrade = 0;
+
+            while (counter <= 12 && badGrade <= 1)
+            {
+                double grade = double.Parse(Console.ReadLine());
+
+                if (grade >= 4)
+                {
+                    sum += grade;
+                    counter++;
+                }
+                else
+                {
+                    badGrade++;
+                }
+            }
+
+            if (badGrade <= 1)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {sum / 12:F2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {counter} grade");
+            }
+        }
+    }
+}
+
