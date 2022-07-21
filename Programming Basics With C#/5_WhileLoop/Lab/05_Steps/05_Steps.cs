cae3a37d6df66d4f62621cd289ade636ea3e2ad4
Dochkoff <hounds.07cinder@icloud.com>
2022-07-22 00:54:33 +0300

    05_Steps

---
+++ Programming Basics With C#/5_WhileLoop/Lab/05_Steps/05_Steps.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05_Steps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSteps = 0;
+
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Going home")
+                {
+                    totalSteps += int.Parse(Console.ReadLine());
+                    break;
+                }
+
+                if (totalSteps < 10000)
+                {
+                    totalSteps += int.Parse(command);
+                }
+
+                if (totalSteps >= 10000)
+                {
+                    break;
+                }
+
+
+            }
+
+            if (totalSteps >= 10000)
+            {
+                Console.WriteLine($"Goal reached! Good job!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000 - totalSteps} more steps to reach goal.");
+            }
+        }
+    }
+}
+
