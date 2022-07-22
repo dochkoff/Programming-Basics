afd55e552847d7f2d3b84e71e276b0ec1b929d4b
Dochkoff <hounds.07cinder@icloud.com>
2022-07-22 18:32:53 +0300

    07_GlassWithWater

---
+++ Programming Basics With C#/5_WhileLoop/Lab/07_GlassWithWater/07_GlassWithWater.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07_GlassWithWater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double glassVolume = double.Parse(Console.ReadLine());
+            double glassFill = 0.0;
+            int counter = 0;
+
+            while (glassFill < glassVolume)
+            {
+                string button = Console.ReadLine();
+
+                if (button == "Easy")
+                {
+                    glassFill += 50;
+                }
+                else if (button == "Medium")
+                {
+                    glassFill += 100;
+                }
+                else if (button == "Hard")
+                {
+                    glassFill += 200;
+                }
+                counter++;
+            }
+
+            if (glassFill == glassVolume)
+            {
+                Console.WriteLine($"The dispenser has been tapped {counter} times.");
+            }
+            else
+            {
+                Console.WriteLine($"{glassFill - glassVolume}ml has been spilled.");
+            }
+        }
+    }
+}
+
