940d4ec6d4facc1b21caefc89c92932b49b9da2b
Dochkoff <hounds.07cinder@icloud.com>
2022-07-23 12:51:52 +0300

    08&09

---
+++ Programming Basics With C#/5_WhileLoop/Lab/08_NumbersInRow/08_NumbersInRow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08_NumbersInRow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int n = 1;
+
+            while (num >= n)
+            {
+                Console.WriteLine(n);
+                n = (n * 2) + 1;
+            }
+        }
+    }
+}
+
---
+++ Programming Basics With C#/5_WhileLoop/Lab/09_Moving/09_Moving.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _09_Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int lenght = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            int quarterVolume = width * lenght * height;
+            int movedVolume = 0;
+
+            while (quarterVolume >= movedVolume)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Done")
+                {
+                    break;
+                }
+
+                int cartons = int.Parse(command);
+                movedVolume += cartons;
+            }
+
+            if (quarterVolume >= movedVolume)
+            {
+                Console.WriteLine($"{quarterVolume - movedVolume} Cubic meters left.");
+            }
+            else
+            {
+                Console.WriteLine($"No more free space! You need {movedVolume - quarterVolume} Cubic meters more.");
+            }
+        }
+    }
+}
+
