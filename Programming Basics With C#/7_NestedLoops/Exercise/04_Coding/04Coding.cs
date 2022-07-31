e9230bb97b88f49cdd0dc4604f8050bebc8cf2a0
Dochkoff <hounds.07cinder@icloud.com>
2022-07-31 22:49:41 +0300

    05_EqualSumsEvenOddPosition

--- Programming Basics With C#/7_NestedLoops/Exercise/04_Coding/04Coding.cs
+++ Programming Basics With C#/7_NestedLoops/Exercise/04_Coding/04Coding.cs
@@ -11,7 +11,7 @@ namespace Coding
             for (int i = 1; i <= n.ToString().Length; i++)
             {
                 int count = n % 10;
-                n = n / 10;
+                n /= 10;
 
                 if (count == 0)
                 {
---
+++ Programming Basics With C#/7_NestedLoops/Exercise/05_EqualSumsEvenOddPosition/05_EqualSumsEvenOddPosition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _05_EqualSumsEvenOddPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+
+            int oddSum = 0;
+            int evenSum = 0;
+            int count = 0;
+
+            for (int i = n1; i <= n2; i++)
+            {
+                for (int x = 0; x < 3; x++)
+                {
+                    count = i % 10;
+                    evenSum += count;
+                    i /= 10;
+
+                    if (i < 10)
+                    {
+                        oddSum += i;
+                    }
+                    else
+                    {
+                        count = i % 10;
+                        oddSum += count;
+                        i /= 10;
+                    }
+                }
+
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+                oddSum = 0;
+                evenSum = 0;
+            }
+        }
+    }
+}
+
