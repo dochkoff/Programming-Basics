f2608b0b94b4592234956e0252f0ad1d043abcb3
Dochkoff <hounds.07cinder@icloud.com>
2022-08-02 23:50:47 +0300

    minor

---
+++ Programming Basics With C#/7_NestedLoops/Exercise/10_PasswordGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _10_PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
