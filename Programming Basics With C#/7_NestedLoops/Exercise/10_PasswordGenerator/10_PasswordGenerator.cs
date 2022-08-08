75ca455789267c8c5a355a1e35d4f5c993108398
Dochkoff <hounds.07cinder@icloud.com>
2022-08-08 18:44:51 +0300

    10_PasswordGenerator

--- Programming Basics With C#/7_NestedLoops/Exercise/10_PasswordGenerator/10_PasswordGenerator.cs
+++ Programming Basics With C#/7_NestedLoops/Exercise/10_PasswordGenerator/10_PasswordGenerator.cs
@@ -6,7 +6,29 @@ namespace _10_PasswordGenerator
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
 
+            for (int x1 = 1; x1 <= n; x1++)
+            {
+                for (int x2 = 1; x2 <= n; x2++)
+                {
+                    for (char x3 = (char)97; x3 <= l + 96; x3++)
+                    {
+                        for (char x4 = (char)97; x4 <= l + 96; x4++)
+                        {
+                            for (int x5 = 1; x5 <= n; x5++)
+                            {
+                                if (x5 > x1 && x5 > x2)
+                                {
+                                    Console.Write($"{x1}{x2}{x3}{x4}{x5} ");
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
